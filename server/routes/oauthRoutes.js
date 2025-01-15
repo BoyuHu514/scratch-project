447b7b7971c22c3dbb7fe942e5912891feadf8df
ssheikhi <fx.sheikhi@gmail.com>
2025-01-15 22:22:12 +0100

    complete login by google Oauth

--- server/routes/oauthRoutes.js
+++ server/routes/oauthRoutes.js
@@ -1,19 +1,17 @@
 import express from 'express';
-import {
-  getAccessToken,
-  getUserData,
-  upsertUser,
-} from '../controllers/oauthController.js';
+import oauthController from '../controllers/oauthController.js';
 
 const router = express.Router();
 
-// 1) Exchange code for an access token
-router.get('/github/access-token', getAccessToken);
+// Exchange code for an access token
+router.get('/github/access-token', oauthController.getAccessToken);
 
-// 2) Get user data from GitHub using the token
-router.get('/github/userdata', getUserData);
+// Get user data from GitHub using the token
+router.get('/github/userdata', oauthController.getUserData);
 
-// 3) Upsert user in local DB
-router.post('/github', upsertUser);
+// Upsert user in local DB
+router.post('/github', oauthController.upsertUser);
+
+router.post('/google', oauthController.googleSignin);
 
 export default router;
